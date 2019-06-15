f5b003ffa17c550a547397aac3ac44f85840f41b
rhgkstjf <44472886+rhgkstjf@users.noreply.github.com>
2019-06-16 02:46:33 +0900

    Update main.py

--- main.py
+++ main.py
@@ -3,7 +3,7 @@ from inte import interprinter
 
 
 
-lastcode = "k = 1; a = 1; while (k equals 0) : k= k-1 finish ; a = k + 2; if(k equals 0) k = k - 1 finish; else k = k+1 finish; print a;"
+lastcode = "y = 5; k = 2; a = 1; while (y equals 1) : y = y-1 finish ; a = k + 2; if(y < 1) y = a finish; else y = k finish; print a;"
 
 code = "while a equals 0 : a = a -1 finish;"
 p = lex_basic(lastcode)
